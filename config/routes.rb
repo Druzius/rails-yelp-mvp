80ef43a8a284c5a804d57ffdea5378d46acc8e7d
Ruben den Hollander <struben@gmail.com>
2018-11-13 20:16:42 +0200

    not yet done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   root to: 'restaurants#index'
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new', as: :restaurant_new
+  # get 'restaurants/:id', to: 'restaurants#show'
+  # post 'restaurants', to: 'restaurants#create'
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '555-61234500',
+    category:     'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '111-61234500',
+    category:     'italian'
+  },
+  {
+    name:         'Manneke Pis',
+    address:      '71 Bowery St, London E2 9UR',
+    phone_number: '333-61234500',
+    category:     'belgian'
+  },
+  {
+    name:         'Shibuya',
+    address:      '90A Oxford St, London E1 1PL',
+    phone_number: '888-61234500',
+    category:     'japanese'
+  },
+  {
+    name:         'Le Wagon Culinaire',
+    address:      '10 Bootcamp Rd, London E4 CU2',
+    phone_number: '777-61234500',
+    category:     'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
